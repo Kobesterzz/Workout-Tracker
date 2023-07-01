fb1a4c729369bfc7b7fca50602c17134ac6384be
kobe shaw <kobeshaw330@gmail.com>
2023-07-01 10:25:59 -0500

    made the workout form

---
+++ work-out-tracker/src/WorkoutForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const WorkoutForm = ({addWorkout}) => {
+    const [ exercise, setExercise ] = useState('');
+    const [ sets, setSets ] = useState(0);
+    const [ reps, setReps ] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const workout = {
+            exercise,
+            sets,
+            reps,
+        };
+
+        addWorkout(workout);
+
+        setExercise('')
+        setSets(0);
+        setReps(0);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+          <h2>Add Workout</h2>
+          <label>
+            Exercise:
+            <input
+              type="text"
+              value={exercise}
+              onChange={(e) => setExercise(e.target.value)}
+            />
+          </label>
+          <label>
+            Sets:
+            <input
+              type="number"
+              value={sets}
+              onChange={(e) => setSets(parseInt(e.target.value))}
+            />
+          </label>
+          <label>
+            Reps:
+            <input
+              type="number"
+              value={reps}
+              onChange={(e) => setReps(parseInt(e.target.value))}
+            />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      );
+}
+
+export default WorkoutForm;

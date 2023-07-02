31ab60527aea3373d0650b6c21f82ebe2ba7d0e9
kobe shaw <kobeshaw330@gmail.com>
2023-07-02 10:55:06 -0500

    made a date part of the list

--- work-out-tracker/src/App.js
+++ work-out-tracker/src/App.js
@@ -5,14 +5,29 @@ import WorkoutList from './WorkoutList';
 const App = () => {
   const [workouts, setWorkouts] = useState([]);
 
-  const addWorkout = (workout) => {
-    setWorkouts([...workouts, workout]);
+  const addWorkout = (date, workout) => {
+    const existingWorkout = workouts.find((w) => w.date === date);
+
+    if (existingWorkout) {
+      existingWorkout.workouts.push(workout);
+      setWorkouts([...workouts]);
+    } else {
+      const newWorkout = {
+        date,
+        workouts: [workout],
+      };
+      setWorkouts([...workouts, newWorkout]);
+    }
+  };
+
+  const handleDateClick = (date) => {
+    console.log('Clicked on date:', date);
   };
 
   return (
     <div>
       <WorkoutForm addWorkout={addWorkout} />
-      <WorkoutList workouts={workouts} />
+      <WorkoutList workouts={workouts} handleDateClick={handleDateClick} />
     </div>
   );
 };
--- work-out-tracker/src/WorkoutList.js
+++ work-out-tracker/src/WorkoutList.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './WorkoutList.sass';
 
-const WorkoutList = ({ workouts }) => {
-    return (
-      <div>
-        <h2>Workout List</h2>
-        {workouts.map((workout, index) => (
-          <div key={index}>
-            <h3>Exercise: {workout.exercise}</h3>
-            <p>Sets: {workout.sets}</p>
-            <p>Reps: {workout.reps}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default WorkoutList;
-  
+
+const WorkoutList = ({ workouts, handleDateClick }) => {
+  return (
+    <div className="workout-list">
+      <h2>Workout List</h2>
+      {workouts.map((workout, index) => (
+        <div className="date-card" key={index} onClick={() => handleDateClick(workout.date)}>
+          <h3>Date: {workout.date}</h3>
+          {workout.workouts.map((w, i) => (
+            <div className="workout-card" key={i}>
+              <h4>Exercise: {w.exercise}</h4>
+              <p>Sets: {w.sets}</p>
+              <p>Reps: {w.reps}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default WorkoutList;

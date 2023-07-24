1c25f7f2e3b31cc5865e0daf59577e139d38649a
kobe shaw <kobeshaw330@gmail.com>
2023-07-24 11:46:15 -0500

    added comment section to the list, and made the list go in a row

--- work-out-tracker/src/WorkoutList.js
+++ work-out-tracker/src/WorkoutList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WorkoutList = ({ workouts, handleDateClick }) => {
   const workoutListStyles = {
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'row',
     alignItems: 'flex-start',
     color: '#2C3333', // Use the primary text color here
   };
@@ -21,7 +21,7 @@ const WorkoutList = ({ workouts, handleDateClick }) => {
     padding: '10px',
     marginBottom: '5px',
     boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    transform: 'translateY(0)',
+    transform: 'translateY(-10)',
     transition: 'transform 0.3s',
   };
 
@@ -39,7 +39,7 @@ const WorkoutList = ({ workouts, handleDateClick }) => {
           key={index}
           onClick={() => handleDateClick(workout.date)}
           onMouseEnter={(e) => {
-            e.currentTarget.style.transform = 'translateY(-10px)';
+            e.currentTarget.style.transform = 'translateY(0px)';
           }}
           onMouseLeave={(e) => {
             e.currentTarget.style.transform = 'translateY(0)';
@@ -52,6 +52,7 @@ const WorkoutList = ({ workouts, handleDateClick }) => {
                 <h4>Exercise: {w.exercise}</h4>
                 <p>Sets: {w.sets}</p>
                 <p>Reps: {w.reps}</p>
+                <p>Notes: {w.notes}</p>
               </div>
             ))}
           </div>

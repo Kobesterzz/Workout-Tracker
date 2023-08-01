e3ecb28c6bf34ede47b148c3f3b60cbf13429c97
kobe shaw <kobeshaw330@gmail.com>
2023-08-01 11:46:11 -0500

    changed the landing page color pallet

--- work-out-tracker/src/LandingPage.js
+++ work-out-tracker/src/LandingPage.js
@@ -7,23 +7,23 @@ const LandingPage = ({ onGetStarted }) => {
     alignItems: 'center',
     justifyContent: 'center',
     height: '100vh',
-    backgroundColor: '#CBE4DE', // Set your desired background color for the landing page
-    color: '#2C3333', // Set your desired text color for the landing page
+    backgroundColor: 'E9A6A6', // Primary color
+    color: '#3F3351', // Accent color
   };
 
   const buttonStyles = {
     padding: '10px 20px',
     fontSize: '16px',
     borderRadius: '5px',
-    backgroundColor: '#2E4F4F', // Set your desired button color
-    color: '#ffffff', // Set your desired button text color
+    backgroundColor: '#3F3351', // Secondary color
+    color: '#ffffff', // Text color for the button
     cursor: 'pointer',
     marginTop: '20px',
   };
 
   return (
     <div style={landingStyles}>
-      <h1>Welcome to the Workout Tracker </h1>
+      <h1>Welcome to the Workout Tracker</h1>
       <p>Keep track of your workouts with ease!</p>
       <button style={buttonStyles} onClick={onGetStarted}>
         Get Started
